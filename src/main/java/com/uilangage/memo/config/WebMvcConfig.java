8e8b822821f1528afd734dc44de96d2f916a2635
DESKTOP-PL3OFLT\user <kangng0806@gmail.com>
2023-10-13 18:40:05 +0900

    [강남규] 인터셉터 추가

--- src/main/java/com/uilangage/memo/config/WebMvcConfig.java
+++ src/main/java/com/uilangage/memo/config/WebMvcConfig.java
@@ -1,18 +1,34 @@
 package com.uilangage.memo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.uilangage.memo.common.FileManager;
+import com.uilangage.memo.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 
 	
+//	@Autowired
+	
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+	}
+	
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		PermissionInterceptor interceptor = new PermissionInterceptor();
+		
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**")
+		.excludePathPatterns("/user/logout", "/static/**", "/images/**");
 	}
 }
